4b5b111038ef5597b02c641ac9ca490cf3c3fd71
bravf <bravfing@126.com>
2018-05-24 11:18:48 +0800

    fix: 修复getNum 区间错误

--- src/components/page/_page.js
+++ src/components/page/_page.js
@@ -5,7 +5,7 @@ var {div, span, rSelect, rSelectOption, rButton} = jsx
 
 function getNumArr(start, end){
   var arr = []
-  for (var i=start; i<end; i++){
+  for (var i=start; i<=end; i++){
     arr.push(i)
   }
   return arr
