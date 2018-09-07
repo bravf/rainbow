95d4b1743ba42f07b27a32b701c2257ec7fcfbbe
bravf <bravfing@126.com>
2018-09-07 15:48:55 +0800

    add: r-app 增加展开收齐左侧菜单功能

--- src/components/app/_app.js
+++ src/components/app/_app.js
@@ -1,21 +1,48 @@
-import {hx} from '../../common/_tools'
 import jsx from '../../common/_jsx'
 
-var {rContainer, rHeader, rAside, rMain} = jsx
+var {rContainer, rHeader, rAside, rMain, div, rIcon} = jsx
 
 var RApp = Vue.extend({
+  props: {
+    expandable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data () {
+    return {
+      isAsideExpand: true,
+    }
+  },
   render (h) {
     jsx.h = h
+    var me = this
+
     return (
       rContainer('.r-app',
         rHeader(
           ...this.$slots.header
         ),
         rContainer('.r-app-body',
-          rAside(
+          rAside({
+            s_display: me.isAsideExpand ? 'block' : 'none',
+            s_left: me.isAsideExpand
+          },
             ...this.$slots.aside
           ),
           rMain(
+            div('.r-aside-expand',{
+              'c_r-aside-expand-true': me.isAsideExpand,
+              'c_r-aside-expand-false': !me.isAsideExpand,
+              vif: this.expandable,
+              o_click () {
+                me.isAsideExpand = !me.isAsideExpand
+              }
+            },
+              rIcon({
+                p_type: me.isAsideExpand ? 'chevron-left' : 'chevron-right'
+              }),
+            ),
             ...this.$slots.main
           )
         )
